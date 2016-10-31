98e5ae53d1e8dce62a3bca0eff92f0a7544f069d
krock <blog@makefile.ovh>
2016-10-31 06:38:02 +0100

    version 0.1

--- src/morpion.py
+++ src/morpion.py
@@ -1,20 +1,53 @@
 
 # -*- coding=utf-8 -*-
-import sys
 from grille import Grille
 
 
 
 def game():
-	print "morpion"
+    print "morpion"
 
-	maGrille = Grille() 
-	maGrille.afficher()
-	maGrille.jouer("rond", 0, 3)
+    maGrille = Grille() 
 
 
 
-
+    while 1:
+        print " "
+        maGrille.afficher()
+        print " "
+        
+        print " "
+        print "joueur croix :"
+        joueur_croix_ligne = input("ligne >> ")
+        joueur_croix_colonne = input("colonne >> ")
+        maGrille.jouer("croix", joueur_croix_ligne, joueur_croix_colonne)
+        if maGrille.verifier_gagner() != "non":
+            print "GAGNANT: " + maGrille.verifier_gagner()
+            print " "
+            maGrille.afficher()
+            print " "
+            break
+        
+        print " "
+        maGrille.afficher()
+        print " "
+        
+        print " "
+        print "joueur rond :"
+        joueur_rond_ligne = input("ligne >> ")
+        joueur_rond_colonne = input("colonne >> ")
+        maGrille.jouer("rond", joueur_rond_ligne, joueur_rond_colonne)
+        if maGrille.verifier_gagner() != "non":
+            print "GAGNANT: " + maGrille.verifier_gagner()
+            print " "
+            maGrille.afficher()
+            print " "
+            break
+        
+        print " "
+        maGrille.afficher()
+        print " "
+        
 
 if __name__ == '__main__':
-	game()
+    game()
